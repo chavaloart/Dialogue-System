ac473b44be73b5d5cb02f43c33ae41ea26a655fa
chavaloart <73241339+chavaloart@users.noreply.github.com>
2020-10-22 15:57:31 +0100

    Add files via upload

---
+++ Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class Dialogue {
+
+	public string[] names;
+
+	[TextArea(3, 10)]
+	public string[] sentences;
+
+    public UnityEvent[] events;
+}
